9ac9646e1d43d10571cd64aa1260fe5c79b23ffe
eryo3 <manuel.porcuna87@gmail.com>
2015-07-31 14:16:39 +0200

    crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -70,3 +70,24 @@ exports.author = function(req, res) {
 	res.render('quizes/author', {});
 
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+
+	var quiz = models.Quiz.build( //crear objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"});
+
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	// guarda en DB los campos pregunta y respuesta de Quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+		res.redirect('/quizes');
+	}) // redireccion HTTP a lista de preguntas
+
+
+};
