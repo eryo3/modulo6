67cbef2fc6d9d608f5d50c191a77f01a0dd3bac5
eryo3 <manuel.porcuna87@gmail.com>
2015-08-18 20:54:13 +0200

    Moderación comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // AUTOLOAD :quizId
+router.param('commentId', commentController.load); // AUTOLOAD :commentId
 
 /*router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);*/
@@ -33,5 +34,5 @@ router.get('/logout', sessionController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 module.exports = router;
